6e3840006f67a6b9c76d313292dc23c2b8d44846
Santiagoarego <48213222+Santiagoarego@users.noreply.github.com>
2019-05-21 11:37:48 -0500

    prueba

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -22,7 +22,7 @@ public class DemoApplication {
     String usuarios="";
     Arreglos sing = Arreglos.constructora();
     for(Usuario us : sing.getUsuarios()){
-      usuarios+=us.getNombre()+" ";
+      usuarios+=us.getNombre()+" "us.getContrasena();
     }
     return usuarios;
   }
